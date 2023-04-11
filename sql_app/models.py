e909392c32d72d96ebc0242bb55a66eb480bf74c
fu-kunn <sky53908@gmail.com>
2023-04-11 18:12:19 +0900

    [Add]Bookingテーブルの作成

--- sql_app/models.py
+++ sql_app/models.py
@@ -18,3 +18,16 @@ class Room(Base):
     room_id = Column(Integer, primary_key=True, index=True)
     room_name = Column(String, unique=True, index=True)
     capacity = Column(Integer)
+
+"""
+ondelete:親が削除された時の処理
+nullable:NULLを許容するか falseの場合は許容しない
+"""
+class Booking(Base):
+    __tablename__ = "bookings"
+    booking_id = Column(Integer, primary_key=True, index=True)
+    room_id = Column(Integer, ForeignKey("rooms.room_id", ondelete="SET NULL"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.user_id", ondelete="SET NULL"), nullable=False)
+    booked_num = Column(Integer)
+    start_datetime = Column(DateTime, nullable=False)
+    end_datetime = Column(DateTime, nullable=False)
