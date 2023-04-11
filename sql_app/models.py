f5af2e00d839e4bbfd91e10e0889e9a60735cb3e
fu-kunn <sky53908@gmail.com>
2023-04-11 18:03:19 +0900

    [Add]Userテーブルの作成

--- sql_app/models.py
+++ sql_app/models.py
@@ -1,1 +1,14 @@
 # データベースの構造・中身
+
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+# database.pyからBaseを読み込む
+from .database import Base
+
+class User(Base):
+    # テーブル名
+    __tablename__ = "users"
+    # カラム名
+    # indexは検索を早くする仕組み
+    user_id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    
