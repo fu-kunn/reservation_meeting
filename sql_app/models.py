f2859f25d2ad957948a345ed133d49773ea537e7
fu-kunn <sky53908@gmail.com>
2023-04-11 18:05:04 +0900

    [Add]Roomテーブルの作成

--- sql_app/models.py
+++ sql_app/models.py
@@ -11,4 +11,10 @@ class User(Base):
     # indexは検索を早くする仕組み
     user_id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
-    
+
+
+class Room(Base):
+    __tablename__ = "rooms"
+    room_id = Column(Integer, primary_key=True, index=True)
+    room_name = Column(String, unique=True, index=True)
+    capacity = Column(Integer)
