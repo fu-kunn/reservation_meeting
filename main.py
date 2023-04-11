08b846e2dc25cddb3d345515b95036c8f1804e41
fu-kunn <sky53908@gmail.com>
2023-04-11 15:29:25 +0900

    [Add]エンドポイントの作成

--- main.py
+++ main.py
@@ -15,7 +15,27 @@ class User(BaseModel):
     user_id: int
     username: str = Field(max_length=12)
 
+
 class Room(BaseModel):
     room_id: int
     room_name: str = Field(max_length=12)
     capacity: int
+
+
+app = FastAPI()
+
+@app.get("/")
+async def index():
+    return {"message": "Succes"}
+
+@app.post("/users")
+async def users(users: User):
+    return {"users": users}
+
+@app.post("/rooms")
+async def rooms(rooms: Room):
+    return {"rooms": rooms}
+
+@app.post("/bookings")
+async def bookings(bookings: Booking):
+    return {"bookings": bookings}
