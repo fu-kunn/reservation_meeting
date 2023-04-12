edb304bcec1611856926d260519ef39fd7ce1970
fu-kunn <sky53908@gmail.com>
2023-04-12 16:49:57 +0900

    [Add]main.pyの修正-getメソッド

---
+++ sql_app/main.py
@@ -0,0 +1,81 @@
+from typing import List
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from . import schemas, models, crud
+from .database import SessionLocal, engine
+# import datetime
+# from pydantic import BaseModel, Field
+
+# スキーマに移行のためコメントアウト
+# class Booking(BaseModel):
+#     booking_id: int
+#     user_id: int
+#     room_id: int
+#     booked_num: int
+#     start_datetime: datetime.datetime
+#     end_datetime: datetime.datetime
+
+
+# class User(BaseModel):
+#     user_id: int
+#     username: str = Field(max_length=12)
+
+
+# class Room(BaseModel):
+#     room_id: int
+#     room_name: str = Field(max_length=12)
+#     capacity: int
+
+# SQLiteのデータベースを作成している
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# セッションを獲得するための関数
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# @app.get("/")
+# async def index():
+#     return {"message": "Succes"}
+
+# Read
+"""
+List[]を使うことで複数のユーザー情報を取得できる
+schemas.Userは1つのユーザー情報
+db: Session = Depends(get_db)→get_db()関数で取得したセッションをdbに入れている
+"""
+@app.get("/users", response_model=List[schemas.User])
+async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+@app.get("/rooms", response_model=List[schemas.Room])
+async def read_rooms(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    rooms = crud.get_rooms(db, skip=skip, limit=limit)
+    return rooms
+
+@app.get("/bookings", response_model=List[schemas.Booking])
+async def read_bookings(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    bookings = crud.get_bookings(db, skip=skip, limit=limit)
+    return bookings
+
+
+# Create
+@app.post("/users")
+async def users(users: User):
+    return {"users": users}
+
+@app.post("/rooms")
+async def rooms(rooms: Room):
+    return {"rooms": rooms}
+
+@app.post("/bookings")
+async def bookings(bookings: Booking):
+    return {"bookings": bookings}
