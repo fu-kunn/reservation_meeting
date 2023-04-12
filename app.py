cf3456fcbefe77e8be716868db54b9ecf91fa01b
fu-kunn <sky53908@gmail.com>
2023-04-12 17:03:51 +0900

    [Add]app.pyの修正-ユーザー登録画面

--- app.py
+++ app.py
@@ -7,19 +7,19 @@ import json
 page = st.sidebar.selectbox("Choose your page", ["users", "rooms", "bookings"])
 
 if page == "users":
-    st.title("APIテスト")
+    st.title("ユーザー登録画面")
     # フォームの作成
     with st.form(key="user"):
-        user_id: int = random.randint(0, 10)
+        # user_id: int = random.randint(0, 10)
         username: str = st.text_input("ユーザー名", max_chars=12)
         data = {
-            "user_id": user_id,
+            # "user_id": user_id,
             "username": username
         }
         submit_button = st.form_submit_button(label="送信")
 
     if submit_button:
-        st.write("## 送信データ")
+        # st.write("## 送信データ")
         st.json(data)
         st.write("##  レスポンス結果")
         url = "http://127.0.0.1:8000/users"
@@ -27,6 +27,8 @@ if page == "users":
             url,
             data=json.dumps(data)
         )
+        if res.status_code == 200:
+            st.success("ユーザー登録完了")
         st.json(res.json())
 
 elif page == "rooms":
